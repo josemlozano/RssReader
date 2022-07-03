3e80f6e4d057e1fde8b9e50fd5563df4593fedd9
josemlozano <josemlozano6@gmail.com>
2022-07-03 11:43:31 +0200

    Initial Commit

---
+++ RssReader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RssReader
+//
+//  Created by Jose Muñoz on 3/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
